58dd26e71b60831a730a7b7c5cc2e57fe79c0760
James Hall <jamesbhall423@byui.edu>
2024-08-28 14:47:58 -0700

    Main Activity

--- app/src/main/java/com/github/jamesbhall423/revelationandroid/MainActivity.java
+++ app/src/main/java/com/github/jamesbhall423/revelationandroid/MainActivity.java
@@ -2,7 +2,13 @@ package com.github.jamesbhall423.revelationandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.github.jamesbhall423.revelationandroid.android.GameActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        final EditText ip = findViewById(R.id.address);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameActivity = new Intent();
+                gameActivity.setClass(MainActivity.this, GameActivity.class);
+                gameActivity.putExtra(GameActivity.IP_OTHER,ip.getText());
+
+                startActivity(gameActivity);
+            }
+        });
     }
 }
